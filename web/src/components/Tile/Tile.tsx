60bd74a121e2f5738104f2111436ba118591e5fe
Diego Cruz <diegoszcruz1001@gmail.com>
2023-02-13 21:50:22 -0300

    init popover

--- web/src/components/Tile/Tile.tsx
+++ web/src/components/Tile/Tile.tsx
@@ -1,5 +1,7 @@
 import styles from './Tile.module.css'
 import { DotsThree } from 'phosphor-react'
+import React, { useState } from 'react';
+import * as Popover from '@radix-ui/react-popover';
 
 type TileProps = {
     tile: {
@@ -12,18 +14,44 @@ type TileProps = {
 }
 
 export function Tile ({ tile }: TileProps) {
+    const [hoverTile, setHoverTile] = useState<boolean>(false)
+    const [hoverContent, setHoverContent] = useState<boolean>(false)
+    const [isOpen, setIsOpen] = useState(false);
+    
+    console.log('HC', hoverContent)
+    console.log('HT', hoverTile)
     const { id, image, name, url } = tile
     return (
         <div className={styles.content}>
             
-            <div className={styles.tile}>
+            <div 
+                className={styles.tile}
+                onMouseEnter={() => setHoverTile(true)}
+                onMouseLeave={() => setHoverTile(false)}
+            >
                 <a href={url} target="_blank">
                     <img src={image} className={styles.image} alt={name} />
                 </a>
                 
-                <div onClick={() => console.log('Options click')} className={styles.optionsTile}>
-                    <p>...</p>
-                </div>
+                <Popover.Root>
+                    <Popover.Trigger asChild className={styles.optionsTile}>
+                        <p>...</p>
+                    </Popover.Trigger>
+
+                    <Popover.Portal>
+                        <Popover.Content 
+                            className={styles.contentPopover}
+                            style={{ display: hoverTile ? 'flex' : 'none' }}
+                            onMouseEnter={() => setHoverContent(true)}
+                            onMouseLeave={() => setHoverContent(false)}
+                        >
+                            <button onClick={() => console.log(`Delete ${tile.id}`)}>Delete Tile</button>
+                            <button onClick={() => console.log(`Edit ${tile.id}`)}>Edit Tile</button>
+                        </Popover.Content>
+
+                    </Popover.Portal>
+                </Popover.Root>
+                
             </div>
 
             <div className={styles.nameTile}>
@@ -31,7 +59,7 @@ export function Tile ({ tile }: TileProps) {
                     {name}
                 </p>
             </div>
-
+            
         </div>
         
     )
